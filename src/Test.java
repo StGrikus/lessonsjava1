c17fa0ed0cf82fca2809607ddf02ed5efd2c4553
Arthur <saintgrikus@gmail.com>
2022-04-05 21:39:45 +0300

    finish lessons27

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,11 @@
+import Forest.*;
+import Forest.Something.SomrClass;
+
+
 public class Test {
     public static void main(String[] args) {
-
+        Tree tree1 = new Tree();
+        Squirell squirell1 = new Squirell();
+        SomrClass someInterfaces = new SomrClass();
     }
 }
