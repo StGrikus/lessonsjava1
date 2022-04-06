c7c7343e6a54723550d67662e66d3978b89d14ae
Arthur <saintgrikus@gmail.com>
2022-04-06 12:41:14 +0300

    finish polimorfizm

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,20 @@
-import Packet1.Person;
-
-public class Test extends Person {
+public class Test {
     public static void main(String[] args) {
         // public - на всем проекте, private - только в приделах одного класса(можно днлать приватные методы),
         // default - модификатор доступа по умолчанию. Доступно в пределах пакета, protected - доступны в пределах
         // одного пакета, эти поля и методы доступны всем подклассам, даже если подклассы вне пакета где они были
         // декларированы
         // класс может быть публичным или деволтным. Класс который совпвдает с названием файла должен быть публичным(!)
+        Animal a1 = new Animal();
+        Dog d1 = new Dog();
+        Cat c1 = new Cat();
+
+        test(a1);
+        test(d1);
+        test(c1);
+    }
+
+    public static void test(Animal animal) {
+        animal.eat();
     }
 }
