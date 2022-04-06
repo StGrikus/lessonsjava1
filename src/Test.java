3f6e685596871e00b90a4d9bb5441bfe5c795264
Arthur <saintgrikus@gmail.com>
2022-04-06 13:03:26 +0300

    finish примитивные типы данных

--- src/Test.java
+++ src/Test.java
@@ -12,6 +12,28 @@ public class Test {
         test(a1);
         test(d1);
         test(c1);
+        System.out.println(" ");
+
+        //lesson30
+
+        float f = (float)123.2; // float f = 123.4f
+        int a = 1234;
+        long l = a; //не явное(делается за нас)
+        int x = (int)l;  //явное
+        //все что int может быть long, но не ноборот. long  2 раза больше чем int
+        //чтобы конвертировать лонг в инт нужно перед лонг указать тип переменной -> int x = (int)l, где l = long
+        System.out.println(x);
+
+        double d = 123.9;
+        int y = (int)d;
+        System.out.println(y);
+
+        long i = Math.round(d);
+        System.out.println(i);
+
+        byte q = (byte)129; // -128 -> 127
+        System.out.println(q);
+
     }
 
     public static void test(Animal animal) {
