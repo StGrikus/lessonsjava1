a26663ebfa2a9fe9f22792eefd04c93090c10a41
Arthur <saintgrikus@gmail.com>
2022-04-13 16:05:29 +0300

    Обработка нескольких исключений урок4

---
+++ src/lesson37/Exeptions4.java
@@ -0,0 +1,18 @@
+package lesson37;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+public class Exeptions4 {
+    public static void main(String[] args) {
+        try {
+            run();
+        } catch (Exception e) {
+
+        }
+    }
+
+    public static void run() throws IOException, ParseException, IllegalArgumentException {
+
+    }
+}
